2e8e091696f9b2d5a0603341b997a0e99683a6d7
Usama <learnbeginners@gmail.com>
2018-11-17 01:24:24 +0500

    Logout user

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -6,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    // To know about logout is working or not.
+    afAuth.authState.subscribe(x => console.log(x));
+  }
 
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
