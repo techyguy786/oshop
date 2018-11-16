f4484cdf5def14b714346d330690724ae25b1d0d
Usama <learnbeginners@gmail.com>
2018-11-17 03:48:42 +0500

    Redirect the User

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from '../auth.service';
 
@@ -13,13 +13,14 @@ export class AuthGuard implements CanActivate {
     ) {}
 
     // https://angular.io/api/router/CanActivate
-    canActivate(): Observable<boolean> {
+    canActivate(router, state: RouterStateSnapshot): Observable<boolean> {
         // Converting Observable of User to Observable of boolean
         return this.auth.user$.pipe(
             map(user => {
                 if (user) { return true; }
 
-                this.router.navigate(['/login']);
+                // Step 1 (redirecting the url)
+                this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
                 return false;
             })
         );
