e2a8a3b2b53ff8f2523ede00e99e0f4b22e8b560
Usama <learnbeginners@gmail.com>
2018-11-16 18:35:54 +0500

    Routes Defined with Normal and Admin User

---
+++ src/app/routes.ts
@@ -0,0 +1,23 @@
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { LoginComponent } from './login/login.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+
+export const appRoute: Routes = [
+    // routes for normal users
+    { path: '', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'shopping-cart', component: ShoppingCartComponent },
+    { path: 'check-out', component: CheckOutComponent },
+    { path: 'order-success', component: OrderSuccessComponent },
+    { path: 'login', component: LoginComponent },
+
+    // routes for admin
+    { path: 'admin/products', component: AdminProductsComponent },
+    { path: 'admin/orders', component: AdminOrdersComponent }
+];
