248fd2846b8c04dd66e033c9ce2c4f1e5efd7f7d
Usama <learnbeginners@gmail.com>
2018-11-16 17:13:40 +0500

    Firebase Auth, DB Setup in project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCUo9T2nglHL8ot7fNFS3GxjqJRfRAr69I',
+    authDomain: 'oshop-f6404.firebaseapp.com',
+    databaseURL: 'https://oshop-f6404.firebaseio.com',
+    projectId: 'oshop-f6404',
+    storageBucket: '',
+    messagingSenderId: '865551720602'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCUo9T2nglHL8ot7fNFS3GxjqJRfRAr69I',
+    authDomain: 'oshop-f6404.firebaseapp.com',
+    databaseURL: 'https://oshop-f6404.firebaseio.com',
+    projectId: 'oshop-f6404',
+    storageBucket: '',
+    messagingSenderId: '865551720602'
+  }
 };
 
 /*
