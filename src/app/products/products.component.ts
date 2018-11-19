ae0c40a95e9a4f54cdd396dcfc170e6c315207fe
Usama <learnbeginners@gmail.com>
2018-11-20 03:16:59 +0500

    Filteration of Products on Main Page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from './../category.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { map } from 'rxjs/operators';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -8,15 +11,36 @@ import { ProductService } from '../product.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
   constructor(
     productService: ProductService,
-    categoryService: CategoryService
+    categoryService: CategoryService,
+    route: ActivatedRoute
   ) {
-    this.products$ = productService.getAll().valueChanges();
-    this.categories$ = categoryService.getAll().valueChanges();
+    productService.getAll().valueChanges().subscribe((products: Product[]) => {
+      this.products = products;
+    });
+    this.categories$ = categoryService.getAll().snapshotChanges()
+                          .pipe(
+                            map(items => {
+                              return items.map(a => {
+                                const data = a.payload.val();
+                                const key = a.payload.key;
+                                return {key, ...data};
+                              });
+                            })
+                          );
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) : this.products;
+    });
   }
 
 }
